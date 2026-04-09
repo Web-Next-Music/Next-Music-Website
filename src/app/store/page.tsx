9638ed6a3786b5d7fb554165b4333f8d069ad89d
Diramix <danyagranny10@proton.me>
2026-04-09 19:54:23 +0300

    fix: StoreFeed import

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/Header";
-import StoreFeed from "@/components/StoreFeed.tsx";
+import StoreFeed from "@/components/StoreFeed";
 import Footer from "@/components/Footer";
 
 export default function Home() {
